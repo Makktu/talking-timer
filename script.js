f62698825d27cc6a708078d67244ce1cd103b71d
Makktu <johnmcna@gmail.com>
2022-01-10 19:19:22 +0000

    added a few things

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
+let displayTime = "00:00";
 let timerOn = false;
 let theTimer = 20;
 
@@ -6,6 +7,8 @@ const timers = [20, 25, 30, 35, 40];
 
 const timerBtn = document.querySelectorAll(".timerBtn");
 const resetBtn = document.querySelector(".reset");
+const timerArea = document.querySelector(".the-timer");
+const twentyMinsVoice = new Audio("sounds/20Minutes.wav");
 
 timerBtn.forEach((btn) => {
     btn.addEventListener("click", (e) => {
@@ -20,15 +23,29 @@ timerBtn.forEach((btn) => {
 resetBtn.addEventListener("click", () => {
     if (timerOn) {
         console.log("timer stopped");
+    } else {
+        startTimer(100);
     }
 });
 
 const startTimer = function (theTimer) {
+    displayTime = "00:00";
+    twentyMinsVoice.play();
     const theInterval = setInterval(function () {
-        console.log(theTimer);
+        if (theTimer < 60) {
+            displayTime = `00:${theTimer > 9 ? theTimer : "0" + theTimer}`;
+        } else {
+            // work out the minutes
+            let minutes = (theTimer / 60 + "").split(".")[0];
+            let seconds = (theTimer / 60 + "").split(".")[1];
+            displayTime = `${minutes}:${seconds}`;
+        }
+        timerArea.textContent = displayTime;
         theTimer--;
         if (theTimer < 0) {
             clearInterval(theInterval);
+            timerArea.textContent = "00:00";
+
             timerOn = false;
             console.log("time's up");
         }
